8ddb7b1a7394b6c133934019fd72e2791aebe66e
Vinicius Bruschi <viniciusbruschi.dev@gmail.com>
2025-04-10 09:27:16 -0400

    Adicionado recursos visuais para página de login

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -6,24 +6,5 @@ use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
-    public function index($value) {
-        return view('main', ['value' => $value]);
-        /*Pode ser feito também da seguinte forma
-            return view('main')->with('value', $value);
-        */
-    }
 
-    public function page2($value) {
-        return view('page2', ['value' => $value]);
-        /*Pode ser feito também da seguinte forma
-            return view('main')->with('value', $value);
-        */
-    }
-
-    public function page3($value) {
-        return view('page3', ['value' => $value]);
-        /*Pode ser feito também da seguinte forma
-            return view('main')->with('value', $value);
-        */
-    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,10 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/main/{value}', [MainController::class, 'index']);
-Route::get('/page2/{value}', [MainController::class, 'page2']);
-Route::get('/page3/{value}', [MainController::class, 'page3']);
+// auth routes
+Route::get('/login', [AuthController::class, 'login']);
+Route::post('/loginSubmit', [AuthController::class, 'loginSubmit']);
+Route::get('/logout', [AuthController::class, 'logout']);
