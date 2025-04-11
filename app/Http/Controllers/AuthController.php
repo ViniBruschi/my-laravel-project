76c8d544adb54a9dd2c093ae1e8735e8b7d000ff
Vinicius Bruschi <viniciusbruschi.dev@gmail.com>
2025-04-11 14:44:07 -0400

    Criadas as regras de validação do login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -11,7 +11,17 @@ class AuthController extends Controller
     }
 
     public function loginSubmit(Request $request) {
-        echo "Logado";
+        $request->validate(
+            [
+                'text_username' => 'required',
+                'text_password' => 'required'
+            ]
+            );
+        
+        $username = $request->input('text_username');
+        $password = $request->input('text_password');
+
+        echo 'OK!';
     }
 
     public function logout() {
